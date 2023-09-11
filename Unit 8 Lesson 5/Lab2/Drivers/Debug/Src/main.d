2c946672fddc531255d07cf99ef6d1dec12c8099
Wessam Gamal <wessamgamal539@gmail.com>
2023-09-11 16:20:27 +0300

    SPI Driver

---
+++ Unit 8 Lesson 5/Lab2/Drivers/Debug/Src/main.d
@@ -0,0 +1,28 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6.h \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_GPIO_Driver.h \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6.h \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_EXTI_Driver.h \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_GPIO_Driver.h \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_USART_Driver.h \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_RCC_Driver.h \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_SPI_Driver.h \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/HAL/includes/lcd.h
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6.h:
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_GPIO_Driver.h:
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6.h:
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_EXTI_Driver.h:
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_GPIO_Driver.h:
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_USART_Driver.h:
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_RCC_Driver.h:
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_SPI_Driver.h:
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/HAL/includes/lcd.h:
