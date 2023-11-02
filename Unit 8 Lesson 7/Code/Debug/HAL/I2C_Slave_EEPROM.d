2e110b48443b72c454c58431b820e769fede7792
Wessam Gamal <wessamgamal539@gmail.com>
2023-11-02 18:42:42 +0200

    I2C Driver

---
+++ Unit 8 Lesson 7/Code/Debug/HAL/I2C_Slave_EEPROM.d
@@ -0,0 +1,16 @@
+HAL/I2C_Slave_EEPROM.o: ../HAL/I2C_Slave_EEPROM.c \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/HAL/includes/I2C_Slave_EEPROM.h \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_I2C_Driver.h \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6.h \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_GPIO_Driver.h \
+ C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_RCC_Driver.h
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/HAL/includes/I2C_Slave_EEPROM.h:
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_I2C_Driver.h:
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6.h:
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_GPIO_Driver.h:
+
+C:/Users/Ahmed\ &\ Wessam/STM32CubeIDE/workspace_1.4.0/Drives/Stm32F103C6_Drives/inc/Stm32_F103C6_RCC_Driver.h:
